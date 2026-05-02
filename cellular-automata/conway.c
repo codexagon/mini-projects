1e033fc613a75fb7dfe7312b732ffffd8c5ab5ab
Rithvik <84326832+rithvikr9@users.noreply.github.com>
2026-05-02 09:19:27 +0530

    Add more featuers to Conway Life sim
    - Play / pause with spacebar
    - Use mouse to edit the grid while simulation is paused

--- cellular-automata/conway.c
+++ cellular-automata/conway.c
@@ -1,33 +1,17 @@
 #include <raylib.h>
+#include <stdbool.h>
 
 #define WIDTH 90
 #define HEIGHT 70
 #define SIZE 10
 
-int grid[WIDTH * HEIGHT] = {0};
-int copy[WIDTH * HEIGHT] = {0};
+bool grid[WIDTH * HEIGHT] = {0};
+bool copy[WIDTH * HEIGHT] = {0};
+
+bool playing = false;
 
 #define cell(row, col) grid[row * WIDTH + col]
 #define cellCopy(row, col) copy[row * WIDTH + col]
-
-void updateGrid() {
-  for (int i = 0; i < WIDTH * HEIGHT; i++) {
-    grid[i] = copy[i];
-  }
-}
-
-void setGlider() {
-	cellCopy(1, 2) = 1;
-	cellCopy(2, 3) = 1;
-	cellCopy(3, 3) = 1;
-	cellCopy(3, 2) = 1;
-	cellCopy(3, 1) = 1;
-}
-
-void setInitialState() {
-	setGlider();
-  updateGrid();
-}
 
 int getAliveCount(int y, int x) {
   int alive = 0;
@@ -36,7 +20,7 @@ int getAliveCount(int y, int x) {
       if (r == y && c == x) {
         continue;
       }
-      if (cell(r, c) == 1) {
+      if (cell(r, c)) {
         alive++;
       }
     }
@@ -45,18 +29,24 @@ int getAliveCount(int y, int x) {
   return alive;
 }
 
+void updateGrid() {
+  for (int i = 0; i < WIDTH * HEIGHT; i++) {
+    grid[i] = copy[i];
+  }
+}
+
 void updateState() {
   for (int r = 1; r < HEIGHT - 1; r++) {
     for (int c = 1; c < WIDTH - 1; c++) {
       int alive = getAliveCount(r, c);
-      if (cell(r, c) == 1) {
+      if (cell(r, c)) {
         if (alive < 2 || alive > 3) {
-          cellCopy(r, c) = 0;
+          cellCopy(r, c) = false;
           continue;
         }
-      } else if (cell(r, c) == 0) {
+      } else {
         if (alive == 3) {
-          cellCopy(r, c) = 1;
+          cellCopy(r, c) = true;
           continue;
         }
       }
@@ -68,25 +58,55 @@ void updateState() {
 void drawGrid() {
   for (int r = 1; r < HEIGHT - 1; r++) {
     for (int c = 1; c < WIDTH - 1; c++) {
-      if (cell(r, c) == 1) {
+      if (cell(r, c)) {
         DrawRectangle(c * SIZE, r * SIZE, SIZE, SIZE, WHITE);
-      } else {
-        DrawRectangle(c * SIZE, r * SIZE, SIZE, SIZE, BLACK);
       }
     }
   }
 }
 
+void drawActiveBorder(int thickness) {
+  if (playing) {
+    DrawLineEx((Vector2){0, 0}, (Vector2){WIDTH * SIZE, 0}, thickness, GREEN);
+    DrawLineEx((Vector2){WIDTH * SIZE, 0},
+               (Vector2){WIDTH * SIZE, HEIGHT * SIZE}, thickness, GREEN);
+    DrawLineEx((Vector2){WIDTH * SIZE, HEIGHT * SIZE},
+               (Vector2){0, HEIGHT * SIZE}, thickness, GREEN);
+    DrawLineEx((Vector2){0, 0}, (Vector2){0, HEIGHT * SIZE}, thickness, GREEN);
+  }
+}
+
+void highlightHoverCell(Vector2 mpos) {
+  DrawRectangle((int)(mpos.x / SIZE) * SIZE, (int)(mpos.y / SIZE) * SIZE, SIZE,
+                SIZE, GRAY);
+}
+
 int main() {
   InitWindow(WIDTH * SIZE, HEIGHT * SIZE, "Conway's Game of Life");
-  SetTargetFPS(4);
+  SetTargetFPS(15);
   SetExitKey(KEY_Q);
-  setInitialState();
 
   while (!WindowShouldClose()) {
-    updateState();
-    ClearBackground(BLACK);
+    Vector2 mousePos = GetMousePosition();
+
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON) && !playing) {
+      cellCopy((int)(mousePos.y / SIZE), (int)(mousePos.x / SIZE)) =
+          !cellCopy((int)(mousePos.y / SIZE), (int)(mousePos.x / SIZE));
+      updateGrid();
+    }
+
+    if (IsKeyPressed(KEY_SPACE)) {
+      playing = !playing;
+    }
+
+    if (playing) {
+      updateState();
+    }
+
     BeginDrawing();
+    ClearBackground(BLACK);
+    highlightHoverCell(mousePos);
+    drawActiveBorder(4);
     drawGrid();
     EndDrawing();
   }
